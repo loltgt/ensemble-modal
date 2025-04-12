319536f2a9b51e7badcf5eddc2f71d733edf7240
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2025-04-12 20:35:05 +0200

    adopt semver, build

--- dist/js/ensemble-modal.min.js
+++ dist/js/ensemble-modal.min.js
@@ -1,6 +1,6 @@
 /*!
  * ensemble Modal
- * @version 0.0.4
+ * @version 0.4.0
  * @link https://github.com/loltgt/ensemble-modal
  * @copyright Copyright (C) Leonardo Laureti
  * @license MIT License
